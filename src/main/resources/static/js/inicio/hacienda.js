de6c6100e2ccecd708ebc109e500999bb891c981
David-1512 <davidjserranom76@gmail.com>
2024-05-27 09:51:36 -0600

    JavaScript de hacienda

--- src/main/resources/static/js/inicio/hacienda.js
+++ src/main/resources/static/js/inicio/hacienda.js
@@ -6,10 +6,10 @@ var stateHacienda ={
 document.addEventListener("DOMContentLoaded",loaded);
 
 async function loaded(event) {
-    document.getElementById("registrarse").addEventListener("click", addPerson);
+    document.getElementById("registrarse").addEventListener("click", existPerson);
     document.getElementById("salirHacienda").addEventListener("click", returnMenu);
 }
-function addPerson() {
+function existPerson() {
     if(document.getElementById('tipo').value === "No seleccionado"){
         alert("Error: No se ha seleccionado tipo de Registro del usuario");
         return;
@@ -22,41 +22,81 @@ function addPerson() {
         alert("Error: Digite el nombre del usuario");
         return;
     }
-    if(document.getElementById('tipo').value === "cliente"){
+    if(document.getElementById('tipo').value === "cliente") {
         stateHacienda.cliente.idCliente = document.getElementById('id').value;
         stateHacienda.cliente.nombreCliente = document.getElementById('nom').value;
-        const request = new Request(backend +`/clientes/${stateClientes.id}`, { method: 'GET', headers: {} });
-        (async ()=>{
-            const response = await fetch(request);
-            if (!response.ok) {
-                errorMessage(response.status);
+        const requestClient = new Request(backend + `/hacienda/cliente/${stateHacienda.cliente.idCliente}`, {
+            method: 'GET',
+            headers: {}
+        });
+        (async () => {
+            const responseClient = await fetch(requestClient);
+            if (!responseClient.ok) {
+                errorMessage(responseClient.status);
                 return;
             }
-            const responseData = await response.json();
-            stateClientes.list = responseData.object;
-            render_listClientes();
-        })();
-        alert("Error: No se ha seleccionado tipo de Registro");
-
-    }
-    if(document.getElementById('tipo').value === "proveedor"){
-        stateHacienda.proveedor.idProveedor = document.getElementById('id').value;
-        stateHacienda.proveedor.nombreProveedor = document.getElementById('nom').value;
-        const request = new Request(backend +`/clientes/${stateClientes.id}`, { method: 'GET', headers: {} });
-        (async ()=>{
-            const response = await fetch(request);
-            if (!response.ok) {
-                errorMessage(response.status);
+            const responseDataClient = await responseClient.json();
+            if (responseDataClient === true) {
+                alert("Error: Este cliente ya esta asociado a hacienda");
                 return;
             }
-            const responseData = await response.json();
-            stateClientes.list = responseData.object;
-            render_listClientes();
+            else{
+                addClienteHacienda();
+            }
         })();
-        alert("Error: No se ha seleccionado tipo de Registro");
-
     }
+        if(document.getElementById('tipo').value === "proveedor"){
+            stateHacienda.proveedor.idProveedor = document.getElementById('id').value;
+            stateHacienda.proveedor.nombreProveedor = document.getElementById('nom').value;
+            const requestProveedor = new Request(backend +`/hacienda/proveedor/${stateHacienda.proveedor.idProveedor}`, { method: 'GET', headers: {} });
+            (async ()=> {
+                const responseProveedor = await fetch(requestProveedor);
+                if (!responseProveedor.ok) {
+                    errorMessage(responseProveedor.status);
+                    return;
+                }
+                const responseDataProveedor = await responseProveedor.json();
+                if (responseDataProveedor === true) {
+                    alert("Error: Este proveedor ya esta asociado a hacienda");
+                    return;
+                }
+                else{
+                    addProveedorHacienda();
+                }
+            })();
+        }
 }
+
+function addProveedorHacienda(){
+    const request = new Request(backend +`hacienda/proveedor`, {method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(stateHacienda.proveedor)});
+    (async ()=>{
+        const response = await fetch(request);
+        if (!response.ok) {errorMessage(response.status);return;}
+        alert("Proveedor asociado correctamente");
+        clean();
+    })();
+}
+
+function addClienteHacienda(){
+    const request = new Request(backend +`hacienda/cliente`, {method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(stateHacienda.cliente)});
+    (async ()=>{
+        const response = await fetch(request);
+        if (!response.ok) {errorMessage(response.status);return;}
+        alert("Cliente asociado correctamente");
+        clean();
+    })();
+}
+
+function clean() {
+    document.getElementById('id').value = "";
+    document.getElementById('nom').value = "";
+    document.getElementById('tipo').value = "No seleccionado";
+}
+
 function returnMenu(){
     document.location="/views/inicio/inicioLogin.html";
 }
